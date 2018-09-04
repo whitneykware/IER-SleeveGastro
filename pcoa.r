6bb5afd54cb0c07048f1319515fde3bd11955c41
Whitney Ware <whitneykware@gmail.com>
2018-09-04 15:56:35 -0400

    new script

--- pcoa.r
+++ pcoa.r
@@ -4,9 +4,9 @@ library("vegan")
 
 rm(list=ls())
 
-project <- "SleeveGastro"
+project <- "IER"
 taxa <- "genus"
-infile <- "SG_genus_taxa.txt"
+infile <- "IER_genus_taxa.txt"
 metaFile <- 'SleeveGastro_Metadata.txt'
 
 myT <- read.table(infile, header=TRUE,sep="\t", row.names = 1)
@@ -20,6 +20,6 @@ myPCOA <- capscale(myT~1,distance="bray")
 merged <- cbind(myT,meta)
 myMerge <- cbind(row.names(myT), merged,myPCOA$CA$u)
 
-#write.table(myPCOA$CA$u, sep="\t", file=paste(project,"_pcoa_",taxa,".txt",sep=""))
+write.table(myPCOA$CA$u, sep="\t", file=paste(project,"_pcoa_",taxa,".txt",sep=""))
 #write.table(myPCOA$CA$eig,file=paste(project,"_eigenValues_pcoa_",taxa,".txt", sep=""), sep="\t")	
 write.table(myMerge , sep="\t", file=paste(project,"_",taxa,"_pcoa_metaMerged.txt", sep=""),row.names=FALSE)
